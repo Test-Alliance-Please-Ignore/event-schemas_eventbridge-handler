ee09d8f5a575994151b6ed7676b9e5ecb76a5196
George <leegeorg07@gmail.com>
2021-03-10 15:58:27 +0000

    Basic error handling

--- eventbridge_handler/client.py
+++ eventbridge_handler/client.py
@@ -14,19 +14,26 @@ class EventHandler:
         sender: str,
         event: BaseModel,
         extra_resources: List[str] = [],
-        detail_type: str = "",
+        detail_type: str = None,
         event_bus: str = None,
     ) -> dict:
+        event_bus = self.event_bus if not event_bus else event_bus
         event = dict(
             Source=sender,
             Detail=event.json(),
             Resources=extra_resources,
-            DetailType=detail_type,
-            EventBusName=self.event_bus if not event_bus else event_bus,
+            DetailType=f"{event_bus}:{self.prefix}",
+            EventBusName=event_bus,
         )
-
-        return self.session.client("events").put_events(
+        send_event = self.session.client("events").put_events(
             Entries=[
                 event,
             ]
         )
+
+        if not send_event.get("FailedEntryCount") == 0:
+            raise Exception(
+                f"{send_event.get('Entries')[0].get('ErrorCode')}: {send_event.get('Entries')[0].get('ErrorMessage')}"
+            )
+
+        return send_event
